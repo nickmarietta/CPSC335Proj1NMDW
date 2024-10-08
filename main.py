621218a4605a93d078006d216d9f6356b8a32ff4
nickmarietta <nick.marietta@csu.fullerton.edu>
2024-10-07 23:35:27 -0700

    fix main program arguments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from left_to_right import leftToRight
 def main():
     print("Give me a number n that will determine how long the list is:\n")
     n = int(input())
-    print("Which algorithm would you like to user? (lawnmower(1) or left_to_right(0))\n")
+    print("Which algorithm would you like to user? (leftToRight(1) or Lawnmower(0))\n")
     if (input() == "1"):
         result = leftToRight(n)
     else:
